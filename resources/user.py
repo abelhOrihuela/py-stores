4aa9708c3db083a4deb1c328ed5ca6278aef0363
Abel Orihuela <abelorihuela@MacBook-Pro.local>
2020-02-12 13:55:18 -0600

    Added flask marshmallow

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
+from flask import request
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
     create_access_token,
@@ -8,44 +9,37 @@ from flask_jwt_extended import (
     jwt_required,
     get_raw_jwt,
 )
+from marshmallow import ValidationError
 from models.user import UserModel
+from schemas.user import UserSchema
 from blacklist import BLACKLIST
 from utils.messages import ERROR_BLANK_FIELD, NOT_FOUND, ERROR_INSERTING
 
-_user_parser = reqparse.RequestParser()
-_user_parser.add_argument(
-    "username", type=str, required=True, help=ERROR_BLANK_FIELD.format("username")
-)
-_user_parser.add_argument(
-    "password", type=str, required=True, help=ERROR_BLANK_FIELD.format("password")
-)
-
+user_schema = UserSchema()
 
 class UserRegister(Resource):
     def post(self):
-        data = _user_parser.parse_args()
 
-        if UserModel.find_by_username(data["username"]):
+        try:
+            data = user_schema.load(request.get_json())
+        except ValidationError as error:
+            return error.messages, 400
+
+        if UserModel.find_by_username(data.username):
             return {"message": "A user with that username already exists."}, 400
 
-        user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
 
 
 class User(Resource):
-    """
-    This resource can be useful when testing our Flask app. We may not want to expose it to public users, but for the
-    sake of demonstration in this course, it can be useful when we are manipulating data regarding the users.
-    """
-
     @classmethod
     def get(cls, user_id: int):
         user = UserModel.find_by_id(user_id)
         if not user:
             return {"message": "User not found."}, 404
-        return user.json(), 200
+        return user_schema.dump(user), 200
 
     @classmethod
     def delete(cls, user_id: int):
@@ -58,12 +52,15 @@ class User(Resource):
 
 class UserLogin(Resource):
     def post(self):
-        data = _user_parser.parse_args()
+        try:
+            user_data = user_schema.load(request.get_json())
+        except ValidationError as error:
+            return error.messages, 400
 
-        user = UserModel.find_by_username(data["username"])
+        user = UserModel.find_by_username(user_data.username)
 
         # this is what the `authenticate()` function did in security.py
-        if user and safe_str_cmp(user.password, data["password"]):
+        if user and safe_str_cmp(user.password, user_data.password):
             # identity= is what the identity() function did in security.py—now stored in the JWT
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
